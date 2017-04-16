87ab32e4a24fdfa23a686111bb9cf66895e61210
Azunyth <alexis.vernot@gmail.com>
2017-04-17 00:18:20 +0200

    fix bug: cannot play video

--- web/router/home/index.js
+++ web/router/home/index.js
@@ -9,19 +9,21 @@ router.get('/', (req, res) => {
 router.get('/random-video', (req, res) => {
     var isAjax = req.xhr;
 
-    req.db.collection('datacontent').aggregate([ { $sample: { size: 1 } } ]).nextObject(function(err, video) {
-        var vid = { iframe: 'Oops error while getting video' };
-        if('IFRAME' in video) {
-            vid.iframe = video.IFRAME;
-        }
-        
-        if(isAjax) {
-            res.json(vid);
-        } else {
-            res.render('random.ejs', vid);
-        }
+    res.json({"iframe":"<iframe src=\"https://www.pornhub.com/embed/ph57d07a0c68096\" frameborder=\"0\" height=\"481\" width=\"608\" scrolling=\"no\"></iframe>"})
 
-    });
+    // req.db.collection('datacontent').aggregate([ { $sample: { size: 1 } } ]).nextObject(function(err, video) {
+    //     var vid = { iframe: 'Oops error while getting video' };
+    //     if('IFRAME' in video) {
+    //         vid.iframe = video.IFRAME;
+    //     }
+    //
+    //     if(isAjax) {
+    //         res.json(vid);
+    //     } else {
+    //         res.render('random.ejs', vid);
+    //     }
+    //
+    // });
 
 });
 
