8349d4d0477ba1b3c7830e68e9859be439c9728d
Azunyth <alexis.vernot@gmail.com>
2017-04-18 23:35:28 +0200

    fix page random video

--- web/router/home/index.js
+++ web/router/home/index.js
@@ -9,20 +9,19 @@ router.get('/', (req, res) => {
 router.get('/random-video', (req, res) => {
     var isAjax = req.xhr;
 
-    res.json({iframe: "\"<iframe src='https://www.youporn.com/embed/11439983/gloryhole/' frameborder='0' height='481' width='608' scrolling='no' name='yp_embed_video'><a href='https://www.youporn.com/watch/11439983/gloryhole/'>Gloryhole!</a> powered by <a href='/'>YouPorn</a>.</iframe><br /><a href='https://www.youporn.com/watch/11439983/gloryhole/'>Gloryhole!</a> powered by <a href='/'>YouPorn</a>.\""});
-    // req.db.collection('datacontent').aggregate([ { $sample: { size: 1 } } ]).nextObject(function(err, video) {
-    //     var vid = { iframe: 'Oops error while getting video' };
-    //     if('IFRAME' in video) {
-    //         vid.iframe = video.IFRAME;
-    //     }
-    //
-    //     if(isAjax) {
-    //         res.json(vid);
-    //     } else {
-    //         res.render('random.ejs', vid);
-    //     }
-    //
-    // });
+    req.db.collection('datacontent').aggregate([ { $sample: { size: 1 } } ]).nextObject(function(err, video) {
+        var vid = { iframe: 'Oops error while getting video' };
+        if('IFRAME' in video) {
+            vid.iframe = video.IFRAME;
+        }
+
+        if(isAjax) {
+            res.json(vid);
+        } else {
+            res.render('random.ejs', vid);
+        }
+
+    });
 
 });
 
