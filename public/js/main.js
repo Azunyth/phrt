bab9f233bb3f1200373aca08e7642be9e59ba677
Azunyth <alexis.vernot@gmail.com>
2017-04-20 21:45:49 +0200

    add fb btn share

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
 
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1409954279075055',
+          xfbml      : true,
+          version    : 'v2.9'
+        });
+        //FB.AppEvents.logPageView();
+
+
+      };
+
+    (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
     var giveme = "Give me another random fap !";
     var wait = 'Randomizing your fap... <span class="loader"></span>';
-    var urlWatch = 'http://faproulette.win/watch/';
+    var baseUrl = 'http://faproulette.win/'
+    var urlWatch = baseUrl + 'watch/';
 
     $("div.btn-launch").on("click", function(e) {
         $(this).siblings('img').eq(0).css({
@@ -22,6 +42,7 @@ $(document).ready(function(){
                     var randomVideoUrl = urlWatch + res.id;
                     $(".share-link-modal a.link-video").attr('href', randomVideoUrl);
                     $(".share-link-modal a.link-video").html(randomVideoUrl);
+                    $("button.btn-fb-share").attr('data-id', res.id);
                 }
             })
         }, 1000)
@@ -44,7 +65,24 @@ $(document).ready(function(){
                 changeIframe(res.iframe);
             });
         }, 600);
-    })
+    });
+
+    $("button.btn-fb-share").on('click', function(e) {
+        var self = $(this);
+        var urlToShare = baseUrl;
+        var videoId = self.attr('data-id');
+
+        if(videoId != undefined) {
+            urlToShare = urlWatch + videoId;
+        }
+
+        FB.ui({
+            method: 'share',
+            display: 'popup',
+            href: urlToShare,
+            quote: "The FapRoulette has choosen for me !"
+          }, function(response){});
+    });
 
     var changeIframe = function(iframe) {
         $("div.iframe-wrapper").empty().append($.parseHTML(checkIFrame(iframe)));
